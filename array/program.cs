f41d877f5d26e74b75f892a9890bffa6fba77ed4
OMI786I <sohrawardy1998@gmail.com>
2026-08-25 22:45:36 +0600

    Clear() and Resize()

--- array/program.cs
+++ array/program.cs
@@ -19,3 +19,33 @@ foreach (var item in pallets2)
 {
     Console.WriteLine($"{item}");
 }
+
+
+
+
+
+
+//using Clear() and Resize() on an array
+
+
+string[] pallets3 =  ["B14", "A11", "B12", "A13" ];
+Console.WriteLine("");
+
+Array.Clear(pallets3, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets3.Length}");
+foreach (var pallet in pallets3)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets3, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets3.Length}");
+
+pallets3[4] = "C01";
+pallets3[5] = "C02";
+
+foreach (var pallet in pallets3)
+{
+    Console.WriteLine($"-- {pallet}");
+}
