23b04aa9fe9f2f2976d16d6fc41dda3874fbf9f1
OMI786I <sohrawardy1998@gmail.com>
2026-08-25 23:33:24 +0600

    completed exercise

--- array/program.cs
+++ array/program.cs
@@ -78,3 +78,28 @@ foreach (string item in items)
 {
     Console.WriteLine(item);
 }
+
+
+
+
+//exercsie: Complete a challenge to reverse words in a sentence
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// Step 1
+string[] message = pangram.Split(' ');
+
+//Step 2
+string[] newMessage = new string[message.Length];
+
+// Step 3
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+}
+
+//Step 4
+string result5 = String.Join(" ", newMessage);
+Console.WriteLine(result5);
