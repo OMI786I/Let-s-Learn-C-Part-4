6192caf3a6e9ae119d4a509bf67062146d173e8a
OMI786I <sohrawardy1998@gmail.com>
2026-08-27 23:07:10 +0600

    string formats

---
+++ string_format/program.cs
@@ -0,0 +1,34 @@
+string first = "Hello";
+string second = "World";
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
+
+//formatting currencies
+
+decimal price2 = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price2:C} (Save {discount:C})");
+
+
+//formatting numbers
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
+
+
+//formatting percentages
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+
+
+
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
