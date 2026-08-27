2f9d68360e86c7e16a2a4d5218e6c49821e45da8
OMI786I <sohrawardy1998@gmail.com>
2026-08-27 23:09:46 +0600

    string interpolation

--- string_format/program.cs
+++ string_format/program.cs
@@ -32,3 +32,20 @@ string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} pri
 
 yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
 Console.WriteLine(yourDiscount);
+
+
+
+
+//string interpolation
+
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
